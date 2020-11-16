57c27e66a3eb51fbc51811cbde9bcc7652bd1a7a
JmbFountain <jmb@smx.de>
2020-11-16 15:15:04 +0100

    bugfixes

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ echo "deploying config files"
 cp ./bash_aliases ~/.bash_aliases
 cp -f ./profile ~/.bashrc
 cp -f ./sources.list /etc/apt/
-cp -f ./apt.conf /etc/apt/
-cp -f ./motd /etc/motd/
+#cp -f ./apt.conf /etc/apt/
+cp -f ./motd /etc/update-motd.d/motd
 cp -f ./sshd_conf /etc/ssh/
+chmod +x /etc/update-motd.d/motd
